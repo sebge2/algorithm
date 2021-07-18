fdaef21a934a2482522dfc511d7da75fb39a65eb
Sébastien Gérard <me@sgerard.be>
2021-07-18 20:14:17 +0200

    matrix order

---
+++ src/main/java/MatrixOrder.java
@@ -0,0 +1,41 @@
+public class MatrixOrder {
+
+    public static int[] order(int[][] matrix) {
+        final int numberRows = matrix.length;
+
+        if (numberRows == 0) {
+            return new int[]{};
+        }
+
+        final int numberColumns = matrix[0].length;
+        if(numberColumns == 0){
+            return new int[]{};
+        }
+
+        final int[] rowPointers = new int[numberRows];
+        final int[] result = new int[numberRows * numberColumns];
+
+        for (int i = 0; i < (numberRows * numberColumns); i++) {
+            final int lowestRowIndex = findLowestRowIndex(rowPointers, matrix);
+
+            result[i] = matrix[lowestRowIndex][rowPointers[lowestRowIndex]];
+            rowPointers[lowestRowIndex]++;
+        }
+
+        return result;
+    }
+
+    private static int findLowestRowIndex(int[] rowPointers, int[][] matrix) {
+        int lowestRowIndex = -1;
+
+        for (int rowPointerIndex = 0; rowPointerIndex < rowPointers.length; rowPointerIndex++) {
+            if(rowPointers[rowPointerIndex] < matrix[0].length){
+                if((lowestRowIndex < 0) || matrix[rowPointerIndex][rowPointers[rowPointerIndex]] < matrix[lowestRowIndex][rowPointers[lowestRowIndex]]){
+                    lowestRowIndex = rowPointerIndex;
+                }
+            }
+        }
+
+        return lowestRowIndex;
+    }
+}
---
+++ src/test/java/MatrixOrderTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class MatrixOrderTest {
+
+    @Test
+    public void order() {
+        final int[][] matrix = new int[][]{
+                new int[]{0, 4, 6, 7},
+                new int[]{2, 8, 12, 15},
+                new int[]{2, 10, 14, 16}
+        };
+
+        final int[] actual = MatrixOrder.order(matrix);
+
+        assertArrayEquals(
+                new int[]{0, 2, 2, 4, 6, 7, 8, 10, 12, 14, 15, 16},
+                actual
+        );
+    }
+
+}
