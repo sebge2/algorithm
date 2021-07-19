9bdeb79bed2c1ac268450038fae244b376878cb1
Sébastien Gérard <me@sgerard.be>
2021-07-19 20:02:24 +0200

    bubble sort

---
+++ src/main/java/BubbleSort.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static int[] bubbleSort(int[] values){
+        for (int i = 0; i < values.length; i++) {
+            for(int j = i -1; j >= 0; j--){
+                if(values[j + 1] < values[j]){
+                    final int temp = values[j + 1];
+
+                    values[j + 1] = values[j];
+                    values[j] = temp;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return values;
+    }
+}
---
+++ src/test/java/BubbleSortTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BubbleSortTest {
+
+    @Test
+    public void sort(){
+        final int[] actual = BubbleSort.bubbleSort(new int[]{7, 1, 8, 9, 3});
+
+        assertArrayEquals(new int[]{1,3,7,8,9}, actual);
+    }
+
+}
